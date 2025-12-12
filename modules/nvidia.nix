650d07761b43a351e5a5e81f06d2f83ae05c6816
alukyano <alukyano@gmail.com>
2025-12-12 17:53:34 +0300

    latest desktop changes

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -1,22 +1,91 @@
-{config, pkgs, ...}: {
-  # for Nvidia GPU
-  #hardware.nvidia.datacenter.enable = true;
+{ config, pkgs, lib, ... }:
 
-  services.xserver.videoDrivers = ["nvidia"];
-  hardware.graphics.enable = true;
-  hardware.nvidia = {
-    modesetting.enable = true;
-    powerManagement.enable = false;
-    powerManagement.finegrained = false;
-    open = false;
-    nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+with lib;
+
+{
+
+  hardware = {
+    nvidia = {
+      modesetting.enable = true;
+      nvidiaPersistenced = true;
+
+      #---------------------------------------------------------------------
+      # Enable the nvidia settings menu
+      #---------------------------------------------------------------------
+      nvidiaSettings = true;
+
+      #---------------------------------------------------------------------
+      # Enable power management (do not disable this unless you have a reason to).
+      # Likely to cause problems on laptops and with screen tearing if disabled.
+      #---------------------------------------------------------------------
+      powerManagement.enable = true;         # Fix Suspend issue
+
+      #---------------------------------------------------------------------
+      # Optionally, you may need to select the appropriate driver version for your specific GPU.
+      #---------------------------------------------------------------------
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
+    };
+
+    #---------------------------------------------------------------------
+    # Direct Rendering Infrastructure (DRI) support, both for 32-bit and 64-bit, and 
+    # Make sure opengl is enabled
+    #---------------------------------------------------------------------
+    opengl = {
+      enable = true;
+      driSupport = true;
+      driSupport32Bit = true;
+
+      #---------------------------------------------------------------------
+      # Install additional packages that improve graphics performance and compatibility.
+      #---------------------------------------------------------------------
+      extraPackages = with pkgs; [        
+        
+        amdvlk
+        intel-media-driver      # LIBVA_DRIVER_NAME=iHD
+        libvdpau-va-gl
+        nvidia-vaapi-driver
+        vaapiIntel                  # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+        vaapiVdpau
+        vulkan-validation-layers
+      ];
+
+    };
   };
-    
-  environment.systemPackages = 
-  with pkgs; [
-    #Packages
-    nvtopPackages.nvidia
+
+  #---------------------------------------------------------------------
+  # OpenRGB && X Server Video Drivers Configuration
+  # Tell Xorg to use the nvidia driver (also valid for Wayland)
+  #---------------------------------------------------------------------
+  services = {
+    hardware.openrgb = {
+      enable = true;
+      motherboard = "intel";
+      package = pkgs.openrgb-with-all-plugins;
+    };
+    xserver.videoDrivers = [ "nvidia" ];
+  };
+
+  #---------------------------------------------------------------------
+  # Set environment variables related to NVIDIA graphics:
+  #---------------------------------------------------------------------
+  environment.variables = {
+    GBM_BACKEND = "nvidia-drm";
+    LIBVA_DRIVER_NAME = "nvidia";
+    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+  };
+
+  #---------------------------------------------------------------------
+  # Packages related to NVIDIA graphics:
+  #---------------------------------------------------------------------
+  environment.systemPackages = with pkgs; [
+
+    clinfo
+    gwe
+    nvtop-nvidia
+    virtualglLib
+    vulkan-loader
+    vulkan-tools
+
   ];
 
 }

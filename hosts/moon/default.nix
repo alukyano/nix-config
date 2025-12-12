6b547adf44eb827a1066cf67f50e7eb7f1085030
alukyano <alukyano@gmail.com>
2025-12-12 22:21:04 +0300

    moon edit

---
+++ hosts/moon/default.nix
@@ -0,0 +1,74 @@
+
+{ config, lib, pkgs, ... }:
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      # Include modules    
+      ../../modules/common.nix
+      ../../modules/gnome-desktop.nix
+      ../../modules/desktop.nix
+      ../../modules/games.nix
+      ../../modules/fonts.nix
+      ../../modules/netbird.nix
+      ../../modules/rustdesk.nix
+      ../../modules/remote.nix
+      ../../modules/virtualisation.nix
+      ../../modules/wine.nix
+      ../../modules/ai_na.nix
+    ];
+
+  system.stateVersion = "25.11";
+  
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  
+  nix.settings.trusted-public-keys = [
+    "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+    "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
+    "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+  ];
+
+  nix.settings.substituters = [
+    "https://cache.nixos.org"
+    "https://nixpkgs-wayland.cachix.org"
+    "https://cuda-maintainers.cachix.org"
+  ];
+ 
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.grub.useOSProber = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  # Swappiness to reduce swapfile usage.
+  boot.kernel.sysctl = { "vm.swappiness" = 10;};
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.tmp.useTmpfs = true;
+
+  swapDevices = [{
+    device = "/swapfile";
+    size = 16 * 1024;
+  }];
+
+  hardware.graphics = {
+    enable = true;
+  };
+
+  networking.hostName = "moon"; # Define your hostname.
+  networking.networkmanager.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.nvidia.acceptLicense = true;
+  
+  programs.nix-ld = {
+    enable = true;
+    libraries = with pkgs; [
+      zlib zstd stdenv.cc.cc curl openssl attr libssh bzip2 libxml2 acl libsodium util-linux xz systemd glib libGL
+    ];
+  };
+
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options ="-delete-older-than 7d";
+  };
+}
+
---
+++ modules/ai_na.nix
@@ -0,0 +1,10 @@
+{pkgs, ...}: {
+    environment.systemPackages = with pkgs; [
+        openai-whisper
+    ];
+
+    services.ollama = { 
+        enable = true; 
+        #acceleration="cuda"; 
+    };
+}

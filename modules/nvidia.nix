7a4e3cf587e7b8e2c5f7369c03a39b5671438722
alukyano <alukyano@gmail.com>
2025-12-12 17:21:13 +0300

    latest desktop changes

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -6,11 +6,11 @@
   hardware.graphics.enable = true;
   hardware.nvidia = {
     modesetting.enable = true;
-    powerManagement.enable = true;
+    powerManagement.enable = false;
     powerManagement.finegrained = false;
     open = false;
     nvidiaSettings = true;
-    #package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
     
   environment.systemPackages = 

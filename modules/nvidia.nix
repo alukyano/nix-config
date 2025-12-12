aef52ed1f614e03324dbb8e2835c2b7018ce2d8e
alukyano <alukyano@gmail.com>
2025-12-12 15:10:34 +0300

    latest desktop changes

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -7,7 +7,7 @@
   hardware.nvidia = {
     modesetting.enable = true;
     powerManagement.enable = true;
-    powerManagement.finegrained = true;
+    powerManagement.finegrained = false;
     open = false;
     nvidiaSettings = true;
     #package = config.boot.kernelPackages.nvidiaPackages.stable;

9cfa0b39f8ccebf3dac9fa7361dbfac1d6f279ae
alukyano <alukyano@gmail.com>
2025-12-26 18:48:45 +0300

    changed ai_na for unstable

--- modules/ai_na.nix
+++ modules/ai_na.nix
@@ -1,6 +1,8 @@
-{pkgs, ...}: {
+{pkgs, pkgs-unstable, ...}: {
     environment.systemPackages = with pkgs; [
         openai-whisper
+        pkgs-unstable.lmstudio
+        pkgs-unstable.vllm
     ];
 
     services.ollama = { 

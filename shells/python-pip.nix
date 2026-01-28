7c2c69abfc3087668a704e77453e104f19bd3e42
alukyano <alukyano@gmail.com>
2026-01-28 22:36:44 +0300

    comfy shell

--- shells/python-pip.nix
+++ shells/python-pip.nix
@@ -14,3 +14,15 @@
   ]);
   runScript = "bash --init-file /etc/profile";
 }).env
+
+# git clone https://github.com/Comfy-Org/ComfyUI.git
+# nix-shell python-pip.nix
+# ln -s /home/alukyano/temp/models /home/alukyano/temp/stable-diffusion-webui-forge/models/Stable-diffusion
+# python3 -m venv venv
+# python -V
+# source venv/bin/activate
+# ./venv/bin/pip install torch torchvision torchaudio
+# ./venv/bin/pip install -r requirements.txt
+# ./venv/bin/pip install -r manager_requirements.txt
+# ./venv/bin/pip install --upgrade pip
+# python main.py --enable-manager --cpu

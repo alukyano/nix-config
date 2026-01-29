39e6104ba9bcda2b835e60074846e23e18a5a1da
alukyano <alukyano@gmail.com>
2026-01-29 13:19:40 +0300

    proxy !!

--- modules/proxy.nix
+++ modules/proxy.nix
@@ -1,7 +1,6 @@
 {
-    default = "http://127.0.0.1:2080";
     networking.proxy.httpProxy = "http://127.0.0.1:2080";
     networking.proxy.httpsProxy = "http://127.0.0.1:2080";
-    networking.proxy.noProxy = "localhost,127.0.0.1,192.168.*.*,.ru"; // Optional: add exceptions
+    networking.proxy.noProxy = "localhost,127.0.0.1,192.168.*.*,.ru";
 }
 

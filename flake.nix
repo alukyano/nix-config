3de6b5fe8e3d3b3ba64eb064450d0ed56209682d
alukyano <alukyano@gmail.com>
2026-01-30 14:56:15 +0300

    11

--- flake.nix
+++ flake.nix
@@ -18,9 +18,6 @@
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
-#    llm-agents.url = "github:numtide/llm-agents.nix";
-#    llm-agents.inputs.nixpkgs.follows = "nixpkgs";
     
     home-manager = {
        url = "github:nix-community/home-manager/release-25.11";

2f78039fa135805fc6f5c9c34c48e51b42d661f7
«alukyano» <alukyano@gmail.com>
2026-01-31 23:17:13 +0300

    xrdp wayland fix

--- modules/xrdp-wayland.nix
+++ modules/xrdp-wayland.nix
@@ -1,11 +1,11 @@
 {lib, config, pkgs, ...}: {
   services.xrdp.enable = true;
-  services.xrdp.defaultWindowManager = "cinnamon-session"; 
-  services.xrdp.defaultSession = "xorg";
+  services.xrdp.defaultWindowManager = "gnome-session"; 
+  #services.xrdp.defaultSession = "xorg";
     # # Optional: tweak xrdp / Xorg configuration if you need
     # # (most setups work out‑of‑the‑box).
-    # extraConfig = ''
-    #   [Xorg]
-    #   pam_auth = /etc/X11/Xwrapper.config
-    # '';
+  extraConfig = ''
+    [Xorg]
+    pam_auth = /etc/X11/Xwrapper.config
+  '';
 }

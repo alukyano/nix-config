5958ae857b9f80ec71446761c5e69d3ee78d6da8
«alukyano» <alukyano@gmail.com>
2026-01-31 23:18:58 +0300

    xorg wayland 2

--- modules/xrdp-wayland.nix
+++ modules/xrdp-wayland.nix
@@ -4,8 +4,8 @@
   #services.xrdp.defaultSession = "xorg";
     # # Optional: tweak xrdp / Xorg configuration if you need
     # # (most setups work out‑of‑the‑box).
-  extraConfig = ''
-    [Xorg]
-    pam_auth = /etc/X11/Xwrapper.config
-  '';
+  # extraConfig = ''
+  #   [Xorg]
+  #   pam_auth = /etc/X11/Xwrapper.config
+  # '';
 }

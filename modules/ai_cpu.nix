e227a3cac000c6bec2186fcdb8a682a12068459b
«alukyano» <alukyano@gmail.com>
2026-01-31 22:05:44 +0300

    ollama host should be without port

--- modules/ai_cpu.nix
+++ modules/ai_cpu.nix
@@ -8,7 +8,7 @@
     ];
 services.ollama = {
      enable = true;
-     host = "0.0.0.0:11434";
+     host = "0.0.0.0";
      openFirewall = true;
      environmentVariables = {
        OLLAMA_CONTEXT_LENGTH = "65536";

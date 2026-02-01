efaf7a6ca1b7b7cc9f032af783a5fc0d16429d5c
«alukyano» <alukyano@gmail.com>
2026-02-01 14:58:32 +0300

    syncthing fix

--- modules/syncthing.nix
+++ modules/syncthing.nix
@@ -6,13 +6,13 @@
     settings = {
         gui = {
           user = "${username}";
-          password = "mypassword";
+          password = "345a79-05445.4s4";
         };
         devices = {
-          "msc-xalukyano" = { id = "DEVICE-ID-GOES-HERE"; };
-          "buldozer" = { id = "DEVICE-ID-GOES-HERE"; };
-          "sputnik" = { id = "DEVICE-ID-GOES-HERE"; };
-          "moon" = { id = "DEVICE-ID-GOES-HERE"; };
+          "msc-xalukyano" = { id = "G6STM5T-ATQZG3W-QGTCNIL-GGTKY5U-VGLWEW6-C673YXU-BBWFJTB-CQES2QZ"; };
+          "buldozer" = { id = "MFXZJ74-NC24DEF-JLLZWFH-EZ6U2RE-2H6KQZ6-WGX6VKQ-Y5ONOQB-5PYZ4AI"; };
+          "sputnik" = { id = "5IW27RD-VIVY3VB-LCIRJOX-72SB4X3-B7OAU3H-TDII57E-BKYDJXD-WUOMMQB"; };
+          "moon" = { id = "7JIOH7R-LAZMVW6-EITU7WR-LKKJNZD-FK2A4EN-PYUABPM-LCRNNJN-5THTZAY"; };
         };
         folders = {
           "Shared" = {

2634d0bae1ac97c77590b42af74e617ccddfd05f
alukyano <alukyano@gmail.com>
2026-02-02 22:00:54 +0300

    disable sputnik bridge

--- hosts/sputnik/default.nix
+++ hosts/sputnik/default.nix
@@ -71,15 +71,15 @@
   networking.hostName = "sputnik"; # Define your hostname.
   networking.networkmanager.enable = true;
   #enp3s0 38:05:25:30:7b:1b
-  networking.interfaces.enp3s0.useDHCP = true;
-  networking.interfaces.br0.useDHCP = true;
-  networking.interfaces.br0.macAddress = "38:05:25:30:7b:1b";
-  networking.bridges = {
-    "br0" = {
-      interfaces = [ "enp3s0" ];
-    };
-  };
-  #networking.ipv6.disable = true;
+#  networking.interfaces.enp3s0.useDHCP = true;
+#  networking.interfaces.br0.useDHCP = true;
+#  networking.interfaces.br0.macAddress = "38:05:25:30:7b:1b";
+#  networking.bridges = {
+#    "br0" = {
+#      interfaces = [ "enp3s0" ];
+#    };
+#  };
+#  networking.ipv6.disable = true;
 
   programs.nix-ld = {
     enable = true;

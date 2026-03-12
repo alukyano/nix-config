1705e295cc82dfe71c4b133dfec2aee41f9efbd5
a.lukyanov <a.lukyanov@gortis.ru>
2026-03-12 14:55:59 +0300

    gitlab.sh init

--- scripts/gitlab.sh
+++ scripts/gitlab.sh
@@ -1,2 +1,14 @@
 #!/usr/bin/env bash
 
+sudo mkdir -p /etc/nixos/gitlab-keys
+
+# Root пароль (30 мин действует)
+echo "GitlabRoot2026!" | sudo tee /etc/nixos/gitlab-root-pass
+
+# Секреты (генерируем)
+for secret in db secret otp jws; do
+  openssl rand -base64 200 | sudo tee /etc/nixos/gitlab-$secret
+done
+
+sudo chmod 600 /etc/nixos/gitlab-*
+sudo chown root:root /etc/nixos/gitlab-*

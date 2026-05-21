70bac21830edd6c7dd68d961bc2a056134e7c195
alukyano <alukyano@gmail.com>
2026-05-21 12:57:09 +0300

    llama-cpp 9264 init

--- overlays/desktop/llama-cpp.nix
+++ overlays/desktop/llama-cpp.nix
@@ -8,37 +8,37 @@ self: super: {
     }).overrideAttrs
       (oldAttrs: rec {
         pname = "llama-cpp";
-        version = "9016";
+        version = "9264";
         src = super.fetchFromGitHub {
           owner = "ggml-org";
           repo = "llama.cpp";
           tag = "b${version}";  
-          hash = "sha256-QhTboBhXQVFyZLlaqXxP254kMq/9idb1fgSsy6pWlvA=";
+          hash = "sha256-LA4SgE20Dvz1g3degdIx4CYfYhVNEIQM5Q/5rDT/icg=";
           leaveDotGit = true;
           postFetch = ''
             git -C "$out" rev-parse --short HEAD > $out/COMMIT
             find "$out" -name .git -print0 | xargs -0 rm -rf
           '';
         };
-        patches = [ ];
-        #vendorHash = "sha256-mQXFTppDI+KgjpZGU40uNOBGNOuMFKXSj3Qld8lTze4=";
-        npmRoot = "tools/server/webui";
-        npmDepsHash = "sha256-k62LIbyY2DXvs7XXbX0lNPiYxuYzeJUyQtS4eA+68f8=";
+        #patches = [ ];
+        vendorHash = "sha256-mQXFTppDI+KgjpZGU40uNOBGNOuMFKXSj3Qld8lTze4=";
+        #npmRoot = "tools/server/webui";
+        #npmDepsHash = "sha256-k62LIbyY2DXvs7XXbX0lNPiYxuYzeJUyQtS4eA+68f8=";
         #npmDepsHash = lib.fakeHash;
 
-        npmDeps = super.fetchNpmDeps {
-          name = "${pname}-${version}-npm-deps";
-          inherit src patches;
-          preBuild = ''
-            pushd ${npmRoot}
-          '';
-          hash = npmDepsHash;
-        };
+        # npmDeps = super.fetchNpmDeps {
+        #   name = "${pname}-${version}-npm-deps";
+        #   inherit src patches;
+        #   preBuild = ''
+        #     pushd ${npmRoot}
+        #   '';
+        #   hash = npmDepsHash;
+        # };
         
-          #prependToVar cmakeFlags "-DLLAMA_BUILD_COMMIT:STRING=$(cat COMMIT)"
-        prePatch = ''
-          touch tools/server/public/index.html.gz
-        '';
+        #   #prependToVar cmakeFlags "-DLLAMA_BUILD_COMMIT:STRING=$(cat COMMIT)"
+        # prePatch = ''
+        #   touch tools/server/public/index.html.gz
+        # '';
 
         preConfigure = ''
           pushd ${npmRoot}
